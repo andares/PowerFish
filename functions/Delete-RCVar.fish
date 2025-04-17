4be23074af1a7a30aa3a6face955f6e75b66a64a
Andares Mer <andares@foxmail.com>
2025-04-18 02:14:56 +0800

    fix remove rc var bug.

--- functions/Delete-RCVar.fish
+++ functions/Delete-RCVar.fish
@@ -9,7 +9,7 @@ function Delete-RCVar -a name
     return $OMF_UNKNOWN_OPT
   end
 
-  koi exec-cmd "sed -i '/^set -xg $name/d' $HOME/.powerrc.fish"
+  koi exec-cmd "sed -i '/^set -xg $name=/d' $HOME/.powerrc.fish"
 
   set -ge $name
 end
