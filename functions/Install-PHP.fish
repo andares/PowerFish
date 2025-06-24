016d652d1f8e5e99bcb843b317736b62af72a8ff
Andares Mer <andares@foxmail.com>
2025-06-25 02:15:56 +0800

    update install php function

--- functions/Install-PHP.fish
+++ functions/Install-PHP.fish
@@ -1,7 +1,3 @@
 function Install-PHP
-  power.Check-Deno || return $OMF_UNKNOWN_ERR
-  # 这里也可以通过`if test $status -ne 0`判断`$status`做出更复杂处理
-
-  deno run -c $POWER_PATH/land/deno.json $POWER_PATH/land/php_helper.ts
-
+  Land install-php
 end
