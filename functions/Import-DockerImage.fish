579cba41d7d1a412bad9614953519b7fb9fe6eec
Andares Mer <andares@foxmail.com>
2025-07-07 03:10:33 +0800

    update

--- functions/Import-DockerImage.fish
+++ functions/Import-DockerImage.fish
@@ -1,5 +1,4 @@
 function Import-DockerImage -a filename tagName
-
   if test -z $filename
     echo Usage: Import-DockerImage \<tagName\> \<filename\>
     return $OMF_MISSING_AGE
@@ -9,26 +8,29 @@ function Import-DockerImage -a filename tagName
   set -l output (docker load -i $filename)
 
   # 提取导入的镜像ID
-  set -l imageId (string match -r 'Loaded image: (.*)' $output | awk '{print $3}')
-  if test -z $tagName; or test "$tagName" = "$imageId"
-    echo "Successfully imported. Image tag: $tagName"
-    return 0
-  end
+  set -l imageId (string trim -- (string match -r 'Loaded image: (.*)' $output | awk '{print $3}'))
+  echo "Successfully imported. Image tag: $imageId"
 
-  # 检查是否成功获取镜像ID
-  if test -z "$imageId"
-    echo "Error: Failed to extract image ID from docker load output"
-    return 1
-  end
+  # INFO: 导入控制tag功能有一堆奇怪的问题，以后再调试
+  # if test -z $tagName; or test "$tagName" = "$imageId"
+  #   echo "Successfully imported. Image tag: $imageId"
+  #   return 0
+  # end
 
-  # 给镜像添加新标签
-  docker tag $imageId $tagName
+  # # 检查是否成功获取镜像ID
+  # if test -z "$imageId"
+  #   echo "Error: Failed to extract image ID from docker load output"
+  #   return 1
+  # end
 
-  # 删除旧标签
-  set -l old_tag (docker image inspect --format '{{range .RepoTags}}{{.}}{{break}}{{end}}' $imageId)
-  if test -n "$old_tag" -a "$old_tag" != "<none>:<none>"
-    docker rmi $old_tag
-  end
+  # # 给镜像添加新标签
+  # docker tag $imageId $tagName
 
-  echo "Successfully imported, retagged as $tagName, and removed old tag"
+  # # 删除旧标签
+  # set -l old_tag (docker image inspect --format '{{range .RepoTags}}{{.}}{{break}}{{end}}' $imageId)
+  # if test -n "$old_tag" -a "$old_tag" != "<none>:<none>"
+  #   docker rmi $old_tag
+  # end
+
+  # echo "Successfully imported, retagged as $tagName, and removed old tag"
 end
