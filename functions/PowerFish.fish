8f0beddf2afdca6927e68ef625b6bc262cdb51d0
Andares Mer <andares@foxmail.com>
2025-07-11 02:13:44 +0800

    update commands help

--- functions/PowerFish.fish
+++ functions/PowerFish.fish
@@ -7,6 +7,15 @@ function PowerFish -d "List commands for PowerFish package."
     echo "Checkout-Repos:签出仓库分支"
     echo "Clone-Repos:克隆仓库到本地"
     echo "Delete-RCVar:移除一个RC文件中的环境变量设置"
+    echo "Docker-Compose:带UID和GID调用docker compose指令"
+    echo "Exec-By:以其他用户执行指令"
+    echo "Export-SecretFile:将加密文件解密后导出到临时文件"
+    echo "Get-DotEnv-Var:获取DotEnv文件中的环境变量值"
+    echo "Make-SecretFile:对文件进行加密存储"
+    echo "Make-SecretLink:将扩展包内的加密文件软链到当前环境"
+    echo "Rebase-Repos:以rebase方式拉取更新仓库"
+    echo "Renew-Repos:放弃所有本地更改然后更新仓库至与远程相同"
+    echo "Show-SecretFile:解密加密文件并输出"
   end | column -t -s ':' -o '  '
 
   #### 这里是老方案搜索文件
