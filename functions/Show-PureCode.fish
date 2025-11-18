f0a4c70af3e52062bfce12e4f276eb4a8ce631e3
Andares Mer <andares@foxmail.com>
2025-11-18 11:39:10 +0800

    update

--- functions/Show-PureCode.fish
+++ functions/Show-PureCode.fish
@@ -1,7 +1,13 @@
-function Show-PureCode -a extension
+# Example with `exclude`: Show-PureCode php "./vendor/*"|wc -l
+function Show-PureCode -a extension exclude
     if test -z "$extension"
         echo "Usage: Show-PureCode <extension>" >&2
         return $OMF_UNKNOWN_ERR
     end
-    find . -type f -name "*.$extension" -print0 | xargs -0 cat | grep -vE '^[[:space:]]*($|//|/\*|\*/|\*|\;|\#|\<\!\-\-)'
+
+    if test -z "$exclude"
+        find . -type f -name "*.$extension" -print0 | xargs -0 cat | grep -vE '^[[:space:]]*($|//|/\*|\*/|\*|\;|\#|\<\!\-\-)'
+    else
+        find . -type f -name "*.$extension" -not -path "$exclude" -print0 | xargs -0 cat | grep -vE '^[[:space:]]*($|//|/\*|\*/|\*|\;|\#|\<\!\-\-)'
+    end
 end
